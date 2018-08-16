0670712eb3869f9542d0e0a3bd06680bb04281af
Unknown <ctran049@west-mec.org>
2018-08-16 14:18:10 -0700

    Final Commit
    Finished validation exercise.

--- snoot.js
+++ snoot.js
@@ -38,6 +38,10 @@ function updateDays() {
     var dates = deliveryDay.getElementsByTagName("option");
     var deliveryMonth = document.getElementById("delivMo");
     var deliveryYear = document.getElementById("delivYr");
+    // cover for no month selected
+    if (deliveryMonth.selectedIndex === -1) {
+        return;
+    }
     var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
     while (dates[28]) {
         deliveryDay.removeChild(dates[28]);
@@ -118,28 +122,192 @@ function validateAddress(fieldsetId) {
         if (currentElement.selectedIndex === -1) {
             currentElement.style.border = "1px solid red";
             fieldsetValidity = false;
-        }
-        else {
+        } else {
             currentElement.style.border = "";
         }
         // action for invalid fieldset
-        if (fieldsetValidity == false) {
+        if (fieldsetValidity === false) {
             if (fieldsetId === "billingAddress") {
                 throw "Please complete all Billing Address information."
-            }
-            else {
+            } else {
                 throw "Please complete all Delivery Address information."
             }
-        }
-        else {
+        } else {
             errorDiv.style.display = "none;"
             errorDiv.innerHTML = "";
         }
-    }
-    catch(msg) {
+    } catch (msg) {
         errorDiv.style.display = "block";
         errorDiv.innerHTML = msg;
         formValidity = false;
+    }
+}
+
+// function to validate delivery date
+function validateDeliveryDate() {
+    var selectElements = document.querySelectorAll("#deliveryDate" + " select");
+    var errorDiv = document.querySelectorAll("#deliveryDate" + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    var elementCount = selectElements.length;
+    var currentElement;
+    try {
+        // loop through select fields looking for blanks
+        for (var i = 0; i < elementCount; i++) {
+            currentElement = selectElements[i];
+            // blanks
+            if (currentElement.value === "") {
+                currentElement.style.border = "1px solid red";
+                fieldsetValidity = false;
+            }
+            // not blanks
+            else {
+                currentElement.style.border = "";
+            }
+        }
+        // action for invalid fieldset
+        if (fieldsetValidity === false) {
+            throw "Please specify a Delivery Date."
+        } else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+}
+
+// function to validate payment
+function validatePayment() {
+    var errorDiv = document.querySelectorAll("#paymentInfo" + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    var ccNumElement = document.getElementById("ccNum");
+    var selectElements = document.querySelectorAll("#paymentInfo" + " select");
+    var elementCount = selectElements.length;
+    var cvvElement = document.getElementById("cvv");
+    var cards = document.getElementsByName("PaymentType");
+    var currentElement;
+    // this is where we were yesterday
+    try {
+        // validate radio buttons one must be on
+        if (!cards[0].checked && !cards[1].checked && !cards[2].checked && !cards[3].checked) {
+            for (var i = 0; i < cards.length; i++) {
+                cards[i].style.outline = "1px solid red";
+            }
+            fieldsetValidity = false;
+        } else {
+            for (var i = 0; i < cards.length; i++) {
+                cards[i].style.outline = "";
+            }
+        }
+        // validate req'd card nbr
+        if (ccNumElement.value === "") {
+            ccNumElement.style.background = "rgb(255,233,233)";
+            formValidity = false;
+        }
+        else {
+            ccNumElement.style.background = "white";
+        }
+        // validate expiration date
+        for (var i = 0; i < elementCount; i++) {
+            currentElement = selectElements[i];
+            // blanks
+            if (currentElement.value === "") {
+                currentElement.style.border = "1px solid red";
+                fieldsetValidity = false;
+            }
+            // not blanks
+            else {
+                currentElement.style.border = "";
+            }
+        }
+        // validate cvv nbr
+        if (cvvElement.value === "") {
+            cvvElement.style.background = "rgb(255,233,233)";
+            formValidity = false;
+        }
+        else {
+            cvvElement.style.background = "white";
+        }
+        // action for invalid fieldset
+        if (fieldsetValidity === false) {
+            throw "Please complete all Payment info.";
+        } else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+}
+
+// function to validate delivery date
+function validateMessage() {
+    var msgBox = document.getElementById("customText");
+    var errorDiv = document.querySelectorAll("#message" + " .errorMessage")[0];
+    var fieldsetValidity = true;
+    try {
+        // validate checkbox and textarea custom message
+        if (document.getElementById("custom").checked && (msgBox.value === "" || msgBox.value === msgBox.placeholder)) {
+            throw "Please enter your Custom Message text.";
+        }
+        else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+            msgBox.style.background = "white";
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        msgBox.style.background = "rgb(255,233,233)";
+    }
+}
+
+// function to validate create account
+function validateCreateAccount() {
+    var errorDiv = document.querySelectorAll("#createAccount" + " .errorMessage")[0];
+    var usernameElement = document.getElementById("username");
+    var pass1Element = document.getElementById("pass1");
+    var pass2Element = document.getElementById("pass2");
+    var invColor = "rgb(255,233,233)";
+    var passwordMismatch = false;
+    var fieldsetValidity = true;
+    usernameElement.style.background = "white";
+    pass1Element.style.background = "white";
+    pass2Element.style.background = "white";
+    errorDiv.style.display = "none";
+    errorDiv.innerHTML = "";
+    try {
+        if (usernameElement.value !== "" && pass1Element.value !== "" && pass2Element.value !== "") { // one or more fields has data
+            if (pass1Element.value !== pass2Element.value) { // verify passwords match
+                fieldsetValidity = false;
+                passwordMismatch = true;
+                throw "Passwords entered do not match, please re-enter.";
+            }
+        }
+        else if (usernameElement.value === "" && pass1Element.value === "" && pass2Element.value === "") { // no fields have data
+            fieldsetValidity = true;
+            passwordMismatch = false;
+        }
+        else {
+            fieldsetValidity = false;
+            throw "Please enter all fields to Create Account.";
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        pass1Element.style.background = invColor;
+        pass2Element.style.background = invColor;
+        formValidity = false;
+        if (passwordMismatch) {
+            usernameElement.style.background = "white";
+        }
+        else {
+            usernameElement.style.background = invColor;
+        }
     }
 }
 
@@ -147,14 +315,17 @@ function validateAddress(fieldsetId) {
 function validateForm(evt) {
     if (evt.preventDefault) {
         evt.preventDefault();
-    }
-    else {
+    } else {
         evt.returnValue = false;
     }
     formValidity = true;
 
     validateAddress("billingAddress");
     validateAddress("deliveryAddress");
+    validateDeliveryDate();
+    validatePayment();
+    validateMessage();
+    validateCreateAccount();
 
     if (formValidity === true) { // form is valid
         document.getElementById("errorText").innerHTML = "";
@@ -163,7 +334,7 @@ function validateForm(evt) {
     } else {
         document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order.";
         document.getElementById("errorText").style.display = "block";
-        scroll(0,0);
+        scroll(0, 0);
     }
 }
 
